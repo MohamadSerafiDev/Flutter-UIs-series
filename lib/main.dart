e969146cfbf1ebf661dae28c1b1393f27049f856
mohamad serafi <mohamad1serafi@gmail.com>
2025-05-26 01:08:02 +0300

    initialize the project

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Force landscape mode only
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+  runApp(const MainApp());
+}
+
+// this project is practicing some unique UIs in flutter
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(body: Center(child: Text('Hello World!'))),
+      ),
+    );
+  }
+}
